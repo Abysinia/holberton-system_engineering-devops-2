4fdcb7b184ab0ed87fbfd135ac0547424e8381f7
Manuelpv17 <manuelpv17@outlook.com>
2020-08-04 13:55:27 -0500

    Re add task 0 create a file with puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 #create a file in /tmp.
 
-file { 'My file':
+file { 'holberton_file':
   ensure  => 'file',
   path    => '/tmp/holberton',
-  group   => 'www-data',
-  mode    => '0744',
   owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet'
 }
