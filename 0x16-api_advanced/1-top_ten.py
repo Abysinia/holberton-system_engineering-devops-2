5907b1ae0ced229620815ab47df39269d4c3760b
Manuelpv17 <manuelpv17@outlook.com>
2020-09-17 23:18:02 -0500

    Add task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Prints the titles of the first 10 hot posts listed
+for a given subreddit. """
+
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """top_ten"""
+
+    response = requests.get(
+        "https://www.reddit.com/r/{}/about.json".
+        format(subreddit), allow_redirects=False,
+        headers={'User-Agent': 'Something here'})
+
+    if response.ok:
+        myData = json.loads(response.text)
+        for post in myData["data"]["children"][:10]:
+            print(post.get('data').get('title'))
+    else:
+        return 0
