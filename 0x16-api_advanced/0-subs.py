1a28b131b63f9b0c7e62e862cbdf8c59f4439b25
Manuelpv17 <manuelpv17@outlook.com>
2020-09-17 22:56:08 -0500

    pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,8 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(
         "https://www.reddit.com/r/{}/about.json".
-        format(subreddit), allow_redirects=False, headers={'User-Agent': 'Something here'})
+        format(subreddit), allow_redirects=False,
+        headers={'User-Agent': 'Something here'})
 
     if response.status_code == 200:
         myData = json.loads(response.text)
