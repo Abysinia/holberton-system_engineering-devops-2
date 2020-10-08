e2c61e9e409ce177e04978828dc6429ed07082e2
Manuelpv17 <manuelpv17@outlook.com>
2020-10-08 08:48:00 -0500

    Correction of small typo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,3 @@
 #  Sky is the limit, let's bring that limit higher
 exec { '/usr/bin/env sed -i "s/500/4096/g" /etc/default/nginx': }
-exec { ''service nginx restart: }
+exec { 'service nginx restart': }
