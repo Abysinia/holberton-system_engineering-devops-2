1498bddd3a92827b1200b70412bfd0061daa58df
Manuelpv17 <manuelpv17@outlook.com>
2020-10-08 09:53:38 -0500

    One line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,2 @@
 #  Sky is the limit, let's bring that limit higher
-exec { '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx': }
-exec { '/usr/bin/env service nginx restart': }
+exec { '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx && service nginx restart': }
