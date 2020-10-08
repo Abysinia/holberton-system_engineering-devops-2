8ed948f7982c742edfdb96dca1b5d1b7e36ed80b
Manuelpv17 <manuelpv17@outlook.com>
2020-10-08 09:26:02 -0500

    Right number to change

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,3 @@
 #  Sky is the limit, let's bring that limit higher
-exec { '/usr/bin/env sed -i s/500/4096/ /etc/default/nginx': }
+exec { '/usr/bin/env sed -i s/15/4096/ /etc/default/nginx': }
 exec { '/usr/bin/env service nginx restart': }
