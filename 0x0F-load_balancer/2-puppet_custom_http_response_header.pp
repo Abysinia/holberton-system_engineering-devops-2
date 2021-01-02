e1d102c59efbcd7f5b2dcd35936267809037786e
Manuelpv17 <manuelpv17@outlook.com>
2021-01-01 20:42:32 -0500

    Add task 2, puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,2 +1,2 @@
 #  custom HTTP header with Puppet
-exec { '/usr/bin/env sed -i "19i '\\\\n\tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-enabled/default': }
+exec { '/usr/bin/env sed -i "19i '\\\\n\tadd_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-enabled/default && sudo service nginx restart': }
