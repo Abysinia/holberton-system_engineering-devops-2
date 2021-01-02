6fc1a1ac81f0a24838f2ceff8e1eecbf1349b626
Manuelpv17 <manuelpv17@outlook.com>
2021-01-01 20:58:28 -0500

    Add task 2, puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,3 @@
 #  custom HTTP header with Puppet
 new_string="\\\\n\tadd_header X-Served-By $HOSTNAME;\n"
-exec { '/usr/bin/env apt-get -y install nginx && sed -i "19i $new_string" /etc/nginx/sites-enabled/default && service nginx restart': }
+exec { '/usr/bin/env apt-get update && apt-get -y install nginx && sed -i "19i $new_string" /etc/nginx/sites-enabled/default && service nginx restart': }
